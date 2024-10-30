6d22b86dcc534865d5724d36c240ce5ed797269c
irelandallison <iealliso@ucsc.edu>
2024-10-29 18:08:07 -0700

    Update lab.js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,22 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - anonymous functions and callbacks
+// Author: Ireland Allison 
+// Date: 10/31/24
 
-// Constants
+function isMultipleOfThree(x) {
+  return (x % 3 == 0); 
+}
+// test function
+console.log("Is 5 a multiple of 3? ", isMultipleOfThree (5));
+console.log("Is 9 a multiple of 3? ", isMultipleOfThree (9));
 
-// Functions
+array = [100, 27, 30, 16, 48, 30000, 90]
+console.log("My array", array);
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-    // some code here
-    // return results;
-  }
-  
-  function main() {
-    console.log("Main function started.");
-    // the code that makes everything happen
-  }
-  
-  // let's get this party started
-  main();
-  
+var result = array.map(isMultipleOfThree);
+// should return [false, true, true, false, true, true, true]
+console.log("Test of multiples of three array:", result);
+
+var result = array.map(function(x){
+  return x ** 0.5;
+})
+//should return [10, 5.196152422706632, 5.477225575051661, 4, 6.928203230275509, 173.20508075688772, 9.486832980505138]
+console.log("Squareroot of array", result);
