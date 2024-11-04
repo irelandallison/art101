61149f0a5cefc83869d5cc9111c7bce50280bd42
irelandallison <iealliso@ucsc.edu>
2024-11-04 11:43:28 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,39 +3,7 @@
 // Date: 11/4/24
 
 // add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-    // some code here
-    // return results;
-  }
-  
-  function main() {
-    console.log("Main function started.");
-    // the code that makes everything happen
-  }
-  
-  // let's get this party started
-  main();
 
