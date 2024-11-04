40eb7cd7c96f3f49a121f4043d16eea9a62a81de
irelandallison <iealliso@ucsc.edu>
2024-11-04 11:37:47 -0800

    new

---
+++ lab9/js/lab.js
@@ -0,0 +1,41 @@
+// index.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+// Author: Ireland Allison
+// Date: 11/4/24
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+
