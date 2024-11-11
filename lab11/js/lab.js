1eb73918bd3fef4cc4c74544b13e67d356fc5737
irelandallison <iealliso@ucsc.edu>
2024-11-10 19:44:50 -0800

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,3 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Events and Forms 
+// Author: Ireland Allison
+// Date: 11/11/24
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+  }
+
+  // click listener for button
+$("#submit").click(function(){
+});
+
+// get value of input field
+const userName = $("#user-name").val();
+
+// now let's sort it
+userNameSorted = sortString(userName);
+
+// append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
