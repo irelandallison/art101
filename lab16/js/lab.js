94cf1af17e6468e61745cd5e782048b11f8883fb
irelandallison <iealliso@ucsc.edu>
2024-12-02 12:30:02 -0800

    new

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,31 +2,32 @@
 // Author: Ireland Allison
 // Date: 12/02/24
 
-// Using the core $.ajax() method
 $.ajax({
-    // The URL for the request (from the api docs)
-    url: "https://xkcd.com/614/info.0.json",
-    // The data to send (will be converted to a query string)
-    data: { 
-            // here is where any data required by the api 
-            //   goes (check the api docs)
-            id: jsonFormatterParsed,
-            api_key: "blahblahblah",
-          },
-    // Whether this is a POST or GET request
+    url: "https://api.allorigins.win/raw?url=https://xkcd.com/info.0.json",
     type: "GET",
-    // The type of data we expect back
-    dataType : "json",
-    // What do we do when the api call is successful
-    //   all the action goes in here
-    success: function(data) {
-        // do stuff
-        console.log(data);
+    dataType: "json",
+    success: function(comicObj) {
+        // Create a section for the comic
+        let outputDiv = $("#output");
+
+        // Create a title using comicObj.title
+        let comicTitle = $("<h3>").text(comicObj.title);
+
+        // Create an image tag using comicObj.img with alt and title attributes
+        let comicImage = $("<img>")
+            .attr("src", comicObj.img)
+            .attr("alt", comicObj.alt)
+            .attr("title", comicObj.alt);
+
+        // Clear any existing content and append new comic elements
+        outputDiv.empty()
+            .append(comicTitle)
+            .append(comicImage);
     },
-    // What we do if the api call fails
-    error: function (jqXHR, textStatus, errorThrown) { 
-        // do stuff
+    error: function(jqXHR, textStatus, errorThrown) {
         console.log("Error:", textStatus, errorThrown);
+        $("#output").text("Sorry, could not retrieve comic.");
     }
-})
+});
+
 
