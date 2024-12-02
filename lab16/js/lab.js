3c636e07069c3401231037eff7598f13279b145e
irelandallison <iealliso@ucsc.edu>
2024-12-02 12:36:07 -0800

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -5,27 +5,23 @@
 $.ajax({
     url: "https://api.allorigins.win/raw?url=https://xkcd.com/info.0.json",
     type: "GET",
-    dataType: "json",
     success: function(comicObj) {
-        // Create a section for the comic
         let outputDiv = $("#output");
 
-        // Create a title using comicObj.title
+        // Create title and image elements
         let comicTitle = $("<h3>").text(comicObj.title);
-
-        // Create an image tag using comicObj.img with alt and title attributes
         let comicImage = $("<img>")
             .attr("src", comicObj.img)
             .attr("alt", comicObj.alt)
             .attr("title", comicObj.alt);
 
-        // Clear any existing content and append new comic elements
+        // Update the output div with the comic
         outputDiv.empty()
             .append(comicTitle)
             .append(comicImage);
     },
     error: function(jqXHR, textStatus, errorThrown) {
-        console.log("Error:", textStatus, errorThrown);
+        console.error("Error:", textStatus, errorThrown);
         $("#output").text("Sorry, could not retrieve comic.");
     }
 });
